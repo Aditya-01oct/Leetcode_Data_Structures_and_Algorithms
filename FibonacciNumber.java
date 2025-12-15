f650431f7f144aae89fc639b1516250bf1bc6842
Aditya Yadav <adityashok989@gmail.com>
2025-12-15 21:57:05 +0530

    Create FibonacciNumber.java

---
+++ FibonacciNumber.java
@@ -0,0 +1,14 @@
+//Brute force to find the fibonacci number.
+class Solution {
+    public int fib(int n) {
+        int a=0,b=1;
+        for(int i=1;i<n;i++){
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        if(n<=1)
+            return n;
+        return b;
+    }
+}
