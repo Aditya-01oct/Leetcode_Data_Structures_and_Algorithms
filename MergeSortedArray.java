9dd96aaa2c472c735d767b38520d23d0c85b1c4e
Aditya Yadav <adityashok989@gmail.com>
2025-12-24 19:10:45 +0530

    Create MergeSortedArray.java

---
+++ MergeSortedArray.java
@@ -0,0 +1,11 @@
+//This is not the optimal approch this is only the brute force approch 
+//This question has to be done again
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
