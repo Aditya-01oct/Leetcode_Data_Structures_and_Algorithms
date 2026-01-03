9910f4b29ba9abb7e2e53b48ded91b6079f2e4dd
Aditya Yadav <adityashok989@gmail.com>
2026-01-03 12:07:42 +0530

    Update MergeSortedArray.java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,5 +1,6 @@
 //This is not the optimal approch this is only the brute force approch 
 //This question has to be done again
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for (int j = 0, i = m; j < n; j++) {
