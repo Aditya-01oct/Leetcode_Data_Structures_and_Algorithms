d9e7c151546922710fa65467439fa15d13a25578
Aditya Yadav <adityashok989@gmail.com>
2026-01-06 23:38:24 +0530

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,10 @@
+//This is a optimized approch and i dont have to update the solution
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0; i<nums.length; i++){
+            ans ^= nums[i];
+        }
+        return ans;    
+    }
+}
