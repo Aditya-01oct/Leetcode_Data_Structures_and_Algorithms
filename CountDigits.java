feee5105b3131b14ecc2ba2d3cc1df251ed40755
Aditya Yadav <adityashok989@gmail.com>
2026-01-13 09:58:29 +0530

    Create CountDigits.java

---
+++ CountDigits.java
@@ -0,0 +1,13 @@
+//This is already a optimized code hence no need to optimize it.
+class Solution {
+    public int countDigits(int num) {
+        int org = num,count = 0;
+        while(org>0){
+            int dig = org%10;
+            if(num%dig == 0)
+                count++;
+            org/=10;
+        }
+        return count;
+    }
+}
